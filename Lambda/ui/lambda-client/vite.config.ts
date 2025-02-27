b1e0e39890f9826c6302fc3f06083a2712c8ca13
DotNotFact <140886710+DotNotFact@users.noreply.github.com>
2025-02-28 01:09:47 +0300

    Настройка конфигов

--- Lambda/ui/lambda-client/vite.config.ts
+++ Lambda/ui/lambda-client/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react(), tsconfigPaths()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'legacy',
+				includePaths: ['src/assets/styles'],
+				silenceDeprecations: ['legacy-js-api']
+			}
+		}
+	},
+	resolve: {
+		alias: {
+			'@/*': 'src/assets/*'
+		}
+	}
 })
